cda79c25426c6a47a622553c2b7bf624a5ad5185
Sefa-Keles <sefakeles.uk@gmail.com>
2026-02-19 04:24:53 +0000

    Update ALLOWED_HOSTS to include 127.0.0.1 for local development

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-blog-app-v1-c32ffdf54eb7.herokuapp.com", ".herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["django-blog-app-v1-c32ffdf54eb7.herokuapp.com", ".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
