41f0076ae7aee81c5a12014bab9e97e1203ac433
Sefa-Keles <sefakeles.uk@gmail.com>
2026-02-22 04:07:37 +0000

    Add Comment model and update Post model; create views and templates for post listing

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
-def blog(request):
-    return HttpResponse("This is blog page")
+# def blog(request):
+#     return HttpResponse("This is blog page")
+
+class PostList(generic.ListView):
+    # model = Post
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
