2a0180a7b49320e74e0b222ec3d1303032f8b32e
Sefa-Keles <sefakeles.uk@gmail.com>
2026-03-03 07:49:38 +0000

    Refactor about_me view to initialize about and collaborate_form after handling POST request

--- about/views.py
+++ about/views.py
@@ -5,8 +5,6 @@ from django.contrib import messages
 # Create your views here.
 
 def about_me(request):
-    about = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm()
 
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
@@ -17,6 +15,9 @@ def about_me(request):
                 messages.SUCCESS,
                 "Collaboration request received! I endeavour to respond within 2 working days."
             )
+    
+    about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
